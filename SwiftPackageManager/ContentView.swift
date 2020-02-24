e8532dfbdb4d260077547674c465595935c8c0fe
SGarcia710 <sgarcia710@outlook.com>
2020-02-24 11:51:19 -0500

    Initial Commit

---
+++ SwiftPackageManager/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftPackageManager
+//
+//  Created by Sebastián on 24/02/20.
+//  Copyright © 2020 SGarcia710. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
